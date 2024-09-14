9fbc2447ca90ad8986a6f18e1754a78f4f90aa6f
Khushi-Malik <khushimalik1910@gmail.com>
2024-09-14 17:15:42 -0400

    added html and css

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('wellrise.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    user_input = request.form['user_input']
-    return render_template('result.html', user_input=user_input)
+# @app.route('/submit', methods=['POST'])
+# def submit():
+#     user_input = request.form['user_input']
+#     return render_template('result.html', user_input=user_input)
 
 if __name__ == '__main__':
     app.run(debug=True)
